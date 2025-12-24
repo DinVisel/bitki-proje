c0e03a68debbaf650d7d06731c09ca59fcbe2be4
Arda Özcan <150556426+DinVisel@users.noreply.github.com>
2025-12-24 10:48:42 +0300

    frontend

---
+++ frontend/Bitki.Blazor/Models/Plant.cs
@@ -0,0 +1,12 @@
+namespace Bitki.Blazor.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LatinName { get; set; }
+        public string Family { get; set; }
+        public string Description { get; set; }
+        public string Habitat { get; set; }
+    }
+}
